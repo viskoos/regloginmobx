7f5e017d1297c22304f8b42388d12e75197a0b66
viskoos <potniye@gmail.com>
2020-03-14 04:52:29 +0600

    refactor(styles): delete sass

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,16 @@ import {Provider} from 'mobx'
 import App from './containers/App'
 import {store} from './store/store'
 
-import './styles/main.scss'
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>
 , document.getElementById('root'));
+
+  // УДАЛИТЬ NODE-SASS(ЕСЛИ НЕ УДАЛИЛСЯ)
+  // ПРОВЕРИТЬ МОБХ
+  // ДВА КОНТЭЙНЕРА ДЛЯ РЕГИ И ЛОГИНА
+  // ПРИХУЯРИТЬ СТИЛИ (ЖЕЛАТЕЛЬНО САССОМ)
+  // ПРОКИНУТЬ ВСЕ ЭТО ДЕЛО ЧЕРЕЗ МОБХ
+  // ПРИХУЯРИТЬ СЕРВЕР
+  // ПАРСИТЬ МОНГОДБ
