c4360d260b0e4d2980e48270034567d3622ef116
viskoos <potniye@gmail.com>
2020-03-14 14:25:43 +0600

    feat(App): parse store data

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {observer, inject} from 'mobx-react'
+import './App.css'
 
 @inject('store')
 @observer
@@ -7,11 +8,24 @@ class App extends Component {
   render() {
 
     const {viewUsers, users} = this.props.store
-    console.log(users)
 
     return(
       <div>
         <button onClick={viewUsers}>view</button>
+        {
+          users.map((item, index) => {
+            return (
+              <div className="user-card" key={index}>
+                <div className="user-card-item">Login: <li>{item.login}</li></div>
+                <div className="user-card-item">Password: <li>{item.password}</li></div>
+                <div className="user-card-item">Name: <li>{item.name}</li></div>
+                <div className="user-card-item">Last Name: <li>{item.lastName}</li></div>
+                <div className="user-card-item">Age: <li>{item.age}</li></div>
+                <div className="user-card-item">ID: <li>{item._id}</li></div>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
